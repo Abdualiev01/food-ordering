432dbc81cd34fe2e043fbb385f864f9cabdb474b
Abrorjon <abdualiev03@inbox.ru>
2023-11-26 11:43:11 +0100

    create a project

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Hero from "@/components/layout/Hero";
+import Header from "@/components/layout/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
---
+++ src/components/layout/Hero.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <section className="grid grid-cols-2">
+      <div>
+        <h1 className="text-4xl font-semibold">
+          Everything is better with a Pizza
+        </h1>
+        <p className="my-4 text-gray-500">
+          Pizza is the missing piece that makes every day complete, a simple yet
+          delicious joy in life
+        </p>
+        <div>
+          <button>Order now</button>
+          <button>learn more</button>
+        </div>
+      </div>
+      <div className="relative">
+        <Image
+          src={"/pizza.png"}
+          layout={"fill"}
+          objectFit="contain"
+          alt="pizza"
+        ></Image>
+      </div>
+    </section>
+  );
+}
