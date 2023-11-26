bb678bd1217103548bc37afbd4d2f324a1f81aa7
Abrorjon <abdualiev03@inbox.ru>
2023-11-26 13:12:11 +0100

    complete a design of hero section

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,19 +1,29 @@
 import Image from "next/image";
+import Right from "../icons/Right";
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-2">
-      <div>
+    <section className="hero">
+      <div className="py-12">
         <h1 className="text-4xl font-semibold">
-          Everything is better with a Pizza
+          Everything
+          <br /> is better
+          <br /> with a&nbsp;
+          <span className="text-primary">Pizza</span>
         </h1>
-        <p className="my-4 text-gray-500">
+        <p className="my-6 text-gray-500 text-sm">
           Pizza is the missing piece that makes every day complete, a simple yet
           delicious joy in life
         </p>
-        <div>
-          <button>Order now</button>
-          <button>learn more</button>
+        <div className="flex gap-4 text-sm">
+          <button className="bg-primary uppercase flex items-center gap-2 text-white rounded-full px-4 py-2">
+            Order now
+            <Right />
+          </button>
+          <button className="flex gap-2 text-gray-600 font-semibold py-2">
+            Learn more
+            <Right />
+          </button>
         </div>
       </div>
       <div className="relative">
