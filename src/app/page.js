b2b975d4c6af950935e3fbace1e083b548b3b8c1
Abrorjon <abdualiev03@inbox.ru>
2023-12-02 06:03:14 +0100

    create a basic design of main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,47 @@
 import Hero from "@/components/layout/Hero";
 import Header from "@/components/layout/Header";
 import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
-      <HomeMenu/>
+      <HomeMenu />
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our Story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-2xl  mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Magna velit nostrud incididunt in do anim quis irure elit nostrud
+            anim. Nisi culpa tempor et est aliqua do nulla aute enim ut proident
+            mollit. Anim ad officia voluptate est amet commodo ad magna dolor
+            sit cillum laborum consectetur esse.
+          </p>
+          <p>
+            Nisi culpa tempor et est aliqua do nulla aute enim ut proident
+            mollit. Anim ad officia voluptate est amet commodo ad magna dolor
+            sit cillum laborum consectetur esse.
+          </p>
+        </div>
+      </section>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a
+            className="text-4xl underline text-gray-500"
+            href="tel:+48577561130"
+          >
+            +48 577 561 130
+          </a>
+        </div>
+      </section>
+      <footer className="border-t-8 p-8 text-center text-gray-500">
+        &copy; 2023 All rights reserved
+      </footer>
     </>
   );
 }
--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import MenuItem from "../Menu/MenuItem";
+import SectionHeaders from "./SectionHeaders";
 
 export default function HomeMenu() {
   return (
@@ -13,11 +14,9 @@ export default function HomeMenu() {
         </div>
       </div>
       <div className="text-center mb-4">
-        <h3 className="uppercase text-gray-500 font-semibold leading-4">
-          Check Out
-        </h3>
-        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+        <SectionHeaders subHeader={'check out'}  mainHeader={'Menu'}/>
       </div>
+
       <div className="grid grid-cols-3 gap-4">
         <MenuItem />
         <MenuItem />
