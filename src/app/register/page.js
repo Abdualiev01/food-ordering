422290da1c417741c047c69b99638ebe37213f85
Abrorjon <abdualiev03@inbox.ru>
2023-12-04 15:30:52 +0100

    create server side for register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,12 +1,35 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function RegisterPage() {
+  const [email, setEmail] = useState("abdualiev1@gmail.ru");
+  const [password, setPassword] = useState("1234343");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
   return (
     <section className="mt-8">
       <h1 className="text-primary text-center text-4xl mb-4">Register</h1>
-      <form className="block max-w-xs mx-auto">
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Register</button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
