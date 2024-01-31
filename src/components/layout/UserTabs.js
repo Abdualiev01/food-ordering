4a6631c9ff906e019de9cc2f793a841972503bf4
Abrorjon <abdualiev03@inbox.ru>
2024-01-31 16:27:50 +0100

    refactoring

--- src/components/layout/UserTabs.js
+++ src/components/layout/UserTabs.js
@@ -1,37 +1,43 @@
-"use client";
+'use client';
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import {usePathname} from "next/navigation";
 
-export default function UserTabs({ isAdmin }) {
+export default function UserTabs({isAdmin}) {
   const path = usePathname();
   return (
     <div className="flex mx-auto gap-2 tabs justify-center flex-wrap">
-      <Link className={path === "/profile" ? "active" : ""} href={"/profile"}>
+      <Link
+        className={path === '/profile' ? 'active' : ''}
+        href={'/profile'}
+      >
         Profile
       </Link>
       {isAdmin && (
         <>
           <Link
-            href={"/categories"}
-            className={path === "/categories" ? "active" : ""}
+            href={'/categories'}
+            className={path === '/categories' ? 'active' : ''}
           >
             Categories
           </Link>
           <Link
-            href={"/menu-items"}
-            className={path.includes("menu-items") ? "active" : ""}
+            href={'/menu-items'}
+            className={path.includes('menu-items') ? 'active' : ''}
           >
             Menu Items
           </Link>
           <Link
-            className={path.includes("/users") ? "active" : ""}
-            href={"/users"}
+            className={path.includes('/users') ? 'active' : ''}
+            href={'/users'}
           >
             Users
           </Link>
         </>
       )}
-      <Link className={path === "/orders" ? "active" : ""} href={"/orders"}>
+      <Link
+        className={path === '/orders' ? 'active' : ''}
+        href={'/orders'}
+      >
         Orders
       </Link>
     </div>
